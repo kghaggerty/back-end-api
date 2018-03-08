a1fceea13bb8113907c6ec6ac00411db82d344ca
Kevin Haggerty <kghaggerty@gmail.com>
2018-03-08 14:59:25 -0600

    added swagger.  can post and get user data

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using back_end_api.Data;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace back_end_api
 {
@@ -31,6 +32,11 @@ namespace back_end_api
             var connection = $"Filename={path}";
             Console.WriteLine($"connection = {connection}");
             services.AddDbContext<back_end_apiContext>(options => options.UseSqlite(connection));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +48,14 @@ namespace back_end_api
             }
 
             app.UseMvc();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
